9efe225fff83b7528f10f0d43197818f4f4d683f
ppdbmafaza-svg <ppdbmafaza@gmail.com>
2025-09-02 08:39:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -30,3 +30,12 @@ document.addEventListener("DOMContentLoaded", () => {
     form.reset();
   });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const toggle = document.getElementById("menu-toggle");
+  const navLinks = document.getElementById("nav-links");
+
+  toggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+});
